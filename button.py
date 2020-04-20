237e9ff14be6b425c2e0c6224d54b4ecdb3b5702
Carlos Arellano <carloscam87@gmail.com>
2020-04-20 19:59:34 +0200

    Addec absolute paths

--- button.py
+++ button.py
@@ -4,34 +4,34 @@ from time import sleep
 
 button = Button(15)
 is_first = True
+last_value = button.value
 
 while True:
     status_tor = os.system('service tor status > log.log')
-    last_value = button.value
- 
+    
     if is_first:
         print('Primera comprobación')
         if status_tor == 0:
-            os.system('./flush_iptables.sh')
-            os.system('./iptables_tor.sh')
+            os.system('/root/tor_leds_button/flush_iptables.sh')
+            os.system('/root/tor_leds_button/iptables_tor.sh')
         else:
-            os.system('./flush_iptables.sh')
-            os.system('./iptables_ap.sh')
+            os.system('/root/tor_leds_button/flush_iptables.sh')
+            os.system('/root/tor_leds_button/iptables_ap.sh')
         is_first = False
 
     if button.value != 0:
         print('Boton pulsado')
         if status_tor == 0:
             os.system('service tor stop')
-            os.system('./flush_iptables.sh')
-            os.system('./iptables_ap.sh')
+            os.system('/root/tor_leds_button/flush_iptables.sh')
+            os.system('/root/tor_leds_button/iptables_ap.sh')
             print('Tor parado')
         else:
-            os.system('./flush_iptables.sh')
-            os.system('./iptables_tor.sh')
+            os.system('/root/tor_leds_button/flush_iptables.sh')
+            os.system('/root/tor_leds_button/iptables_tor.sh')
             os.system('service tor start')
             print('Tor encendido')
-        sleep(3)
+        sleep(4)
 
 
 
